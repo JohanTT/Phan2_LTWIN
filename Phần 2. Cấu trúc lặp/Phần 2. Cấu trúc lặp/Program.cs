1108d350e62bc8a3c0d16f884075d76708d3bfdb
Cao Viet Thang <thejohan39@gmail.com>
2021-09-30 15:25:24 +0700

    Phan 2 cua 120 bai Ltwin

---
+++ Phần 2. Cấu trúc lặp/Phần 2. Cấu trúc lặp/Program.cs
@@ -0,0 +1,313 @@
+﻿using System;
+
+namespace Phần_2._Cấu_trúc_lặp
+{
+    class Program
+    {
+        // ______________________________BAI 24______________________________
+        static void Bai24()
+        {
+            Console.Write("Nhap so nguyen n: ");
+            int n = int.Parse(Console.ReadLine());
+            int dem = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    dem++;
+                }
+            }
+            if (dem == 2)
+            {
+                Console.WriteLine("n la so nguyen to");
+            }
+            else
+            {
+                Console.WriteLine("n ko là so nguyen to");
+            }
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 25______________________________
+        static int UCLN(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+        }
+        static int BCNN(int a, int b)
+        {
+            return a * b / UCLN(a, b);
+        }
+        static void Bai25()
+        {
+            Console.Write("Nhap a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("UCLN cua 2 so la " + UCLN(a, b));
+            Console.WriteLine("BCNN cua 2 so la " + BCNN(a, b));
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 26______________________________
+        static void Bai26()
+        {
+            Console.Write("Nhap vao 1 so nguyen duong N: ");
+            int N = int.Parse(Console.ReadLine());
+            int dem = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                    dem++;
+            }
+            Console.WriteLine("Dem so uoc: " + dem);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 27______________________________
+        static int SOHOANHAO(int n)
+        {
+            int flag = 0, sum = 0;
+
+            //Tìm tổng ước số   
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0) sum += i;
+
+            }
+            Console.Write("Tong cua cac uoc so: {0}", sum);
+            //So sánh tổng ước số với số đã cho để tìm ra số hoàn hảo
+            if (sum == n & n != 0) flag = 1;
+            return flag;
+        }
+
+        static void Bai27()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int check = SOHOANHAO(n);
+            if (check == 1) Console.WriteLine("la so hoan thien");
+            else Console.WriteLine("khong la so hoan thien");
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 28______________________________
+        static void Bai28()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int gt = 1;
+
+            int i = 0;
+
+            for (i = 1; i <= n; ++i)
+                gt = gt * i;
+            Console.WriteLine("n!= " + gt);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 29______________________________
+        static int TinhTich(int n)
+        {
+            int p = 1;
+            for (int i = 0; i <= n; i++)
+                p = p * (2 * i + 1);
+            return p;
+        }
+
+        static void Bai29()
+        {
+            Console.Write("Nhap so nguyen n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 0)
+                Console.WriteLine("Ket qua: " + TinhTich(n));
+            else Console.WriteLine("SAI NHAP LAI");
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 30______________________________
+        static int TinhTong(int n)
+        {
+            int tong = 0;
+            for (int i = 0; i <= n; i++)
+                tong = tong + (2 * i + 1);
+            return tong;
+        }
+        static void Bai30()
+        {        
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 0)
+                Console.WriteLine("Ket qua: " + TinhTong(n));
+            else
+                Console.WriteLine("SAI NHAP LAI");
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 31______________________________
+        static long TinhCongTru(int n)
+        {
+            long p = 0;
+            long tmp = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                p += tmp * i;
+                tmp *= -1;
+            }
+            return p;
+        }
+
+        static void Bai31()
+        {
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+                Console.WriteLine("Ket qua: " + TinhCongTru(n));
+            else
+                Console.WriteLine("SAI NHAP LAI");
+            Console.ReadKey();
+
+        }
+
+        // ______________________________BAI 32______________________________
+        static int TinhCongNhan(int n)
+        {
+
+            int tong = 0, tich = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                tich = tich * i;
+                tong = tong + tich;
+            }
+            return tong;
+        }
+
+        static void Bai32()
+        {
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+                Console.WriteLine("Ket qua: " + TinhCongNhan(n));
+            else Console.WriteLine("SAI NHAP LAI");
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 33______________________________
+        static void Bai33()
+        {
+            long S = 0;
+            Console.WriteLine("Nhap vao so n: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                S = S + i * i;
+            }
+            Console.WriteLine("Tong 1^2 + 2^2 + ... + n^2 la: {0}", S);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 34______________________________
+        static void Bai34()
+        {
+            Console.WriteLine("Nhap vao so n: ");
+            int n = int.Parse(Console.ReadLine());
+            float S = 0;
+            while (n <= 0) ;
+            for (int i = 1; i <= n; i++)
+            {
+                S += 1 / (float)i;
+            }
+            Console.WriteLine("Tong 1 + 1/2 + 1/3 + ... + 1/n la: {0}", S);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 35______________________________
+        static void Bai35()
+        {
+            Console.WriteLine("Nhap vao so n: ");
+            int n = int.Parse(Console.ReadLine());
+            float S = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                S = S + i * i;
+            }
+            Console.WriteLine("Tong 1 + 2^2 + 3^2 + ... + n^2 la: {0}", S);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 36______________________________
+        static void Bai36()
+        {
+            Console.WriteLine("Nhap vao so n: ");
+            int n = int.Parse(Console.ReadLine());
+            float S = 0;
+            for (int i = 1; i <= n; i++) // với i = 1, S = 0;
+            {
+                S = S + (float)i / (i + 1);
+            }
+            Console.WriteLine("Tong 1/2 + 2/3 + 3/4 + n/(n+1) la: {0}", S);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 37______________________________
+        //tao ham mu tu dinh nghia
+        public static double Tinh(int n)
+        {
+            double s = 0;
+            long p = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                p = p * i;
+                s = s + (double)1 / p;
+            }
+            return s;
+        }
+        static void Bai37()
+        {
+            Console.WriteLine("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tong 1 + 1/2! + 1/3! + ... + 1/n! la: {0}", Tinh(n));
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Bai24();
+
+            Bai25();
+
+            Bai26();
+
+            Bai27();
+
+            Bai28();
+
+            Bai29();
+
+            Bai30();
+
+            Bai31();
+
+            Bai32();
+
+            Bai33();
+
+            Bai33();
+
+            Bai34();
+
+            Bai35();
+
+            Bai36();
+
+            Bai37();
+
+
+        }
+    }
+}
---
+++ Phần 2. Cấu trúc điều kiện/Phần 2. Cấu trúc điều kiện/Program.cs
@@ -0,0 +1,300 @@
+﻿using System;
+
+namespace Phần_2._Cấu_trúc_điều_kiện
+{
+    class Program
+    {
+        // ______________________________BAI 12______________________________
+        static void Bai12()
+        {
+            Console.WriteLine("Nhap a va b: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int max = a;
+            if (max > a) Console.WriteLine(max);
+            max = b;
+            if (max > b) Console.WriteLine(max);
+            Console.WriteLine("So lon nhat la: " + max);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 13______________________________
+        static void Bai13()
+        {
+            Console.WriteLine("Nhap a va b: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int max = a;
+            if (max > a) Console.WriteLine(max);
+            max = b;
+            if (max > b) Console.WriteLine(max);
+            max = c;
+            if (max > c) Console.WriteLine(max);
+            Console.WriteLine("So lon nhat la: " + max);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 14______________________________
+        static void Bai14()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int tm = 0;
+            if (a > b)
+            {
+                tm = a;
+                a = b;
+                b = tm;
+            }
+            if (a > c)
+            {
+                tm = a;
+                a = c;
+                c = tm;
+            }
+            if (b > c)
+            {
+                tm = b;
+                b = c;
+                c = tm;
+            }
+            Console.WriteLine(" Thu tu tang dan la: " + a + " " + b + " " + c);
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 15______________________________
+        static void Bai15()
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            if (A == 0)
+            {
+                if (B == 0)
+                {
+                    Console.WriteLine("Phuong trinh vo so nghiem");
+                }
+                else
+                {
+                    Console.WriteLine("Phuong trinh vo nghiem");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh co nghiem: " + (-B / A));
+            }
+
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 16______________________________
+        static void Bai16()
+        {
+            float a, b, c;
+            Console.WriteLine("Nhap a : ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b : ");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap c : ");
+            c = float.Parse(Console.ReadLine());
+            float delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem : ");
+                Console.WriteLine("X1 = {0}", ((-b - Math.Sqrt(delta)) / 2 * a));
+                Console.WriteLine("X2 = {0}", ((-b + Math.Sqrt(delta)) / 2 * a));
+            }
+            else if (delta == 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem kep ");
+                Console.WriteLine("X1 = X2 {0}", -b / 2 * a);
+            }
+            else if (delta < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 17______________________________
+        static void Bai17()
+        {
+            Console.WriteLine("Nhap a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap c: ");
+            int c = int.Parse(Console.ReadLine());
+            double denta = b * b - 4 * a * c;
+            if (a == 0 && b != 0 && c != 0)
+            {
+                double A1 = Math.Sqrt(-c / b);
+                double A2 = -Math.Sqrt(-c / b);
+                if ((-c / b) > 0)
+                    Console.WriteLine("Phuong trinh co 2 nghiem {0}", A1, A2);
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            if (a == 0 && b == 0 && c != 0)
+                Console.WriteLine("Phuong trinh vo nghiem");
+            if (a == 0 && b == 0 && c == 0)
+                Console.WriteLine("Phuong trinh vo so nghiem");
+            if (a != 0 && b == 0 && c == 0 || a == 0 && b != 0 && c == 0)
+                Console.WriteLine("Phuong trinh co 1 nghiem =0");
+            if (a != 0 && b != 0 || a != 0 && c != 0)
+            {
+                if (denta < 0)
+                    Console.WriteLine("Phuong trinh vo nghiem");
+                if (denta == 0)
+                {
+                    double t = -b / (2 * a);
+                    double X1 = Math.Sqrt(t);
+                    double X2 = -Math.Sqrt(t);
+                    if (t < 0)
+                        Console.WriteLine("Phuong trinh vo nghiem");
+                    else if (t >= 0)
+                        if (t == 0)
+                            Console.WriteLine("Phuong trinh 1 co nghiem = 0");
+                        else
+                            Console.WriteLine("Phuong trinh co 2 nghiem {0} {1}", X1, X2);
+                }
+                if (denta > 0)
+                {
+                    double t1 = ((-b + Math.Sqrt(denta)) / (2 * a));
+                    double t2 = ((-b - Math.Sqrt(denta)) / (2 * a));
+                    double x1 = Math.Sqrt(t1);
+                    double x2 = -Math.Sqrt(t1);
+                    double x3 = Math.Sqrt(t2);
+                    double x4 = -Math.Sqrt(t2);
+                    if (t1 < 0 && t2 < 0)
+                        Console.WriteLine("Phuong trinh vo nghiem");
+                    if (t1 == 0 && t2 < 0 || t2 == 0 && t1 < 0)
+                        Console.WriteLine("Phuong trinh 1 co nghiem = 0");
+                    if (t2 == 0 && t1 > 0)
+                        Console.WriteLine("Phuong trinh co 3 nghiem = 0, {0}, {1}", x1, x2);
+                    if (t1 == 0 && t2 > 0)
+                        Console.WriteLine("Phuong trinh co 3 nghiem = 0, {0}, {1}", x3, x4);
+                    if (t1 > 0 && t2 < 0)
+                        Console.WriteLine("Phuong trinh co 2 nghiem = {0}, {1}", x1, x2);
+                    if (t1 < 0 && t2 > 0)
+                        Console.WriteLine("Phuong trinh co 2 nghiem = {0}, {1}", x3, x4);
+                    if (t1 > 0 && t2 > 0)
+                        Console.WriteLine("Phuong trinh co 4 nghiem = {0}, {1}, {2}, {3}", x1,
+                       x2, x3, x4);
+                }
+            }
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 18______________________________
+        static void Bai18()
+        {
+            Console.WriteLine("Nhap so nguyen n gom 3 chu so: ");
+            int n = int.Parse(Console.ReadLine());
+            int tram = n / 100;
+            int chuc = ((n % 100) / 10);
+            int donvi = (n % 10);
+            int max = tram;
+            if (tram > chuc)
+            {
+                if (tram > donvi)
+                {
+                    Console.WriteLine("Chu so lon nhat nam o hang tram. ");
+                }
+                else
+                {
+                    Console.WriteLine("Chu so lon nhat nam o hang don vi. ");
+                }
+            }
+            else if (chuc > donvi)
+                Console.WriteLine("Chu so lon nhat nam o hang chuc. ");
+            else
+                Console.WriteLine("Chu so lon nhat nam o hang don vi. ");
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 19______________________________
+        static void Bai19()
+        {
+            Console.WriteLine("Nhap so nguyen n gom 3 chu so: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = n / 100;
+            int b = ((n % 100) / 10);
+            int c = (n % 10);
+            if (a > b)
+            {
+                int doi = a;
+                a = b;
+                b = doi;
+            }
+            if (b > c)
+            {
+                int doi = b;
+                b = c;
+                c = doi;
+            }
+            if (a > b)
+            {
+                int doi = a;
+                a = b;
+                b = doi;
+            }
+            Console.WriteLine("Thu tu tang dan cua cac chu so {0}, {1}, {2}", a, b, c);
+
+            Console.ReadKey();
+        }
+
+        // ______________________________BAI 20______________________________
+        static void Bai20()
+        {
+
+        }
+
+        // ______________________________BAI 21______________________________
+        static void Bai21()
+        {
+
+        }
+
+        // ______________________________BAI 22______________________________
+        static void Bai22()
+        {
+
+        }
+
+        // ______________________________BAI 23______________________________
+        static void Bai23()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Bai12();
+
+            Bai13();
+
+            Bai14();
+
+            Bai15();
+
+            Bai16();
+
+            Bai17();
+
+            Bai18();
+
+            Bai19();
+
+            Bai20();
+
+            Bai21();
+
+            Bai22();
+
+            Bai23();
+        }
+    }
+}
